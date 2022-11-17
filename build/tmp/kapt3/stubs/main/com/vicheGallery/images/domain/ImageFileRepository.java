80c6fdce7cc048e3930a79fc280e54096be6fe1e
최민호 <ssamzag@gmail.com>
2022-11-17 12:53:35 +0900

    delete build

--- build/tmp/kapt3/stubs/main/com/vicheGallery/images/domain/ImageFileRepository.java
+++
@@ -1,8 +0,0 @@
-package com.vicheGallery.images.domain;
-
-import java.lang.System;
-
-@kotlin.Metadata(mv = {1, 6, 0}, k = 1, d1 = {"\u0000\u0012\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0010\t\n\u0000\bg\u0018\u00002\u000e\u0012\u0004\u0012\u00020\u0002\u0012\u0004\u0012\u00020\u00030\u0001\u00a8\u0006\u0004"}, d2 = {"Lcom/vicheGallery/images/domain/ImageFileRepository;", "Lorg/springframework/data/jpa/repository/JpaRepository;", "Lcom/vicheGallery/images/domain/ImageFile;", "", "vicheGallery"})
-@org.springframework.stereotype.Repository()
-public abstract interface ImageFileRepository extends org.springframework.data.jpa.repository.JpaRepository<com.vicheGallery.images.domain.ImageFile, java.lang.Long> {
-}

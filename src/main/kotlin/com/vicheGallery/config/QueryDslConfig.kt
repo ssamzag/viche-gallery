62d13f7b8983563930661f5dcbf25c5d8009cd7c
ssamzag <ssamzag@gmail.com>
2022-12-17 12:47:39 +0900

    feat: querydsl 설정파일 추가

---
+++ src/main/kotlin/com/vicheGallery/config/QueryDslConfig.kt
@@ -0,0 +1,18 @@
+package com.vicheGallery.config
+
+import com.querydsl.jpa.impl.JPAQueryFactory
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import javax.persistence.EntityManager
+import javax.persistence.PersistenceContext
+
+@Configuration
+class QueryDslConfig (
+    @PersistenceContext
+    private val em: EntityManager
+){
+    @Bean
+    fun jpaQueryFactory(): JPAQueryFactory {
+        return JPAQueryFactory(em)
+    }
+}
